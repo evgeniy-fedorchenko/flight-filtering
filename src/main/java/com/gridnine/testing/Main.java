46c01703a3c4bd0772bf6f326d3abbfad36a55b7
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-07-27 21:16:44 +0500

    refactor output in main

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -5,6 +5,7 @@ import com.gridnine.testing.filter.FilterFactory;
 import com.gridnine.testing.flight.Flight;
 import com.gridnine.testing.flight.FlightBuilder;
 
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -12,8 +13,12 @@ public class Main {
     public static void main(String[] args) {
 
         List<Flight> flights = FlightBuilder.createFlights();
-        List<Filter> filters = new FilterFactory().getFilters();
+        Collection<Filter> filters = new FilterFactory().getFilters();
+        String viewPattern = "Фильтр: %s\nРезультат: %s\nОтфильтровано полётов: %s\n\n";
 
+        filters.forEach(f -> {
+            Collection<Flight> result = f.doFilter(flights);
+            System.out.printf(viewPattern, f.getName(), result, flights.size() - result.size());
+        });
     }
-
 }
